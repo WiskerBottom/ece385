ab63e772edc806c3ced4b7acea13fbba16d3b8b1
RoYHa683 <ryounis2@illinois.edu>
2026-02-24 19:50:46 -0600

    asdf

---
+++ lab5/lab5.srcs/sim_1/new/testbench.sv
@@ -0,0 +1,56 @@
+module testbench();
+
+timeunit 10ns;
+timeprecision 1ns;
+
+
+logic		clk;
+
+logic 		reset;
+
+logic 		run_i;
+logic 		continue_i;
+logic [15:0] sw_i;
+
+logic [15:0] led_o;
+logic [7:0]  hex_seg_left;
+logic [3:0]  hex_grid_left;
+logic [7:0]  hex_seg_right;
+logic [3:0]  hex_grid_right;
+
+
+
+processor_top processor_top0 (.*);
+
+initial begin: CLOCK_INITIALIZATION 
+    clk = 1'b1;
+end
+
+always begin: CLOCK_GENERATION
+    #1 clk = ~clk;
+end
+
+initial begin 
+
+reset = 1;
+
+#10;
+
+reset = 0;
+
+#100
+
+continue_i <= 1;
+
+#10
+
+continue_i <= 0;
+
+#100
+
+
+
+$finish();
+
+end 
+endmodule
